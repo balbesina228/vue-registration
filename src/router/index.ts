42f60ad9e3459e1914b907818f7fc25055255a38
balbesina228 <dolphie.guy@gmail.com>
2025-05-19 00:07:43 +0700

    (feature) add profile view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import RegisterView from '@/views/RegisterView.vue';
+import ProfileView from '@/views/ProfileView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {  path: '/', name: 'Register', component: RegisterView }
+  { path: '/', redirect: '/register' },
+  { path: '/register', component: RegisterView },
+  { path: '/profile', component: ProfileView },
 ];
 
 const router = createRouter({
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="profile-view">
+    <span class="title">Здравствуйте, NICKNAME</span>
+    <a href="/" class="quit">Выход</a>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.profile-view {
+  width: 615px;
+  height: 220px;
+  align-items: center;
+  justify-content: center;
+  background-color: #fffffe;
+  padding: 42px;
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  gap: 57px;
+}
+
+.title {
+  font-weight: 500;
+  font-size: 40px;
+  display: block;
+  margin-bottom: 24px;
+}
+
+.quit {
+  text-decoration: underline;
+  text-underline-offset: 3px;
+  color: #2b2a29
+}
+</style>
